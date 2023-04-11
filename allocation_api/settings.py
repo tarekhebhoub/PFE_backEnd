27ab8dbef84119cd9d9aa664b6e064ca1e337373
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-04-11 23:15:00 +0100

    updates to zinou branch

--- allocation_api/settings.py
+++ allocation_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-06%vi#+uq&km&y8$7d1%v%pdd%a*(c!!pux%nxljilv)xoh#eh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["172.0.0.1","192.168.100.7","localhost"]
 
 
 # Application definition
