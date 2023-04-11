f6e476ca25d6a4a55a4d5d836a362967076de5a2
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-04-11 23:50:18 +0100

    test

--- api/apiviews.py
+++ api/apiviews.py
@@ -11,20 +11,22 @@ from django.shortcuts import get_object_or_404
 
 import hashlib
 
-from .models import User,Pos_User,Station
-from .serializers import UserSerializer,LoginSerializer,PositionSerializer,StationSerializer
+from .models import User, Pos_User, Station
+from .serializers import UserSerializer, LoginSerializer, PositionSerializer, StationSerializer
+
 
 class UserCreate(generics.CreateAPIView):
-    authentication_classes=()
-    permission_classes=()
-    serializer_class=UserSerializer
-    def post(self,request):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
+
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = User.objects.create_user(**serializer.validated_data)
-            token=Token.objects.create(user=user)
+            token = Token.objects.create(user=user)
             serializer = UserSerializer(user)
-            return Response({"token":user.auth_token.key,"username":user.username},status=status.HTTP_201_CREATED)
+            return Response({"token": user.auth_token.key, "username": user.username}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=400)
         # try:
     #     user=User(request.data)
@@ -34,41 +36,42 @@ class UserCreate(generics.CreateAPIView):
     # #     last_name=request.data.get('last_name'),
     # # )
     # # user.set_password(request.data.get('password'))
-   
+
     #     user.save()
     #     token=Token.objects.create(user=user)
     #     return Response({"token":user.auth_token.key,"username":user.username},status=status.HTTP_201_CREATED)
-        # except:
-        #     return Response("username already exists",status.HTTP_400_BAD_REQUEST)
-    
-class LoginView(APIView):
-    permission_classes=()
-    serializer_class=LoginSerializer
-    def post(self,request):
-        username=request.data.get("username")
-        password=request.data.get("password")
+    # except:
+    #     return Response("username already exists",status.HTTP_400_BAD_REQUEST)
 
-        user=authenticate(username=username,password=password)
+
+class LoginView(APIView):
+    permission_classes = ()
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
         if user:
             try:
                 Token.objects.create(user=user)
-                return Response({"token":user.auth_token.key,"username":user.username})
+                return Response({"token": user.auth_token.key, "username": user.username})
             except:
                 return Response({'token already exists'})
         else:
-            return Response({"error":"Wrong Credentials"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LogoutView(APIView):
     def post(self, request):
         Token.objects.filter(user=request.user).delete()
-        return Response({"Logout successfully"},status=status.HTTP_200_OK)
- 
-
+        return Response({"Logout successfully"}, status=status.HTTP_200_OK)
 
 
 class Lock(APIView):
-   
-    def crypt_lock(self,matricule):
+
+    def crypt_lock(self, matricule):
         # plaintext = matricule
         # key = Fernet.generate_key()
         # plaintext_bytes = plaintext.encode()
@@ -77,101 +80,105 @@ class Lock(APIView):
         # encrypted_string = encrypted_bytes.decode()
         # return encrypted_string
 
-
         message = matricule
         hash_object = hashlib.sha256(message.encode())
         hash_hex = hash_object.hexdigest()
         return hash_hex
-        
-    def get(self,request):
-        lock=self.crypt_lock(request.user.matricule)
-        return Response({"lock":lock})
+
+    def get(self, request):
+        lock = self.crypt_lock(request.user.matricule)
+        return Response({"lock": lock})
 
 
 class Position(APIView):
-    serializer_class=PositionSerializer
-    def get(self,request):
-        pos_user=Pos_User.objects.get(user=request.user.id)
-        serializer=PositionSerializer(pos_user)
-        
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    def post(self,request):
+    serializer_class = PositionSerializer
+
+    def get(self, request):
+        pos_user = Pos_User.objects.get(user=request.user.id)
+        serializer = PositionSerializer(pos_user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
         # data=JSONParser().parse(request.data)
-        data={'user':request.user.id}
+        data = {'user': request.user.id}
         data.update(request.data)
-        serializer=PositionSerializer(data=data)
+        serializer = PositionSerializer(data=data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    def put(self,request):
-        pos_user=Pos_User.objects.get(user=request.user.id)
-        pos_user.latitude=request.data.get("latitude")
-        pos_user.longitude=request.data.get("longitude")
-        
-        pos_user.save()
-        serializer=PositionSerializer(pos_user)
-        
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-class StationsView(APIView):
-    serializer_class=StationSerializer
-    def get(self,request):
-        station = Station.objects.all()
-        serializer=StationSerializer(station,many=True)
-        
-        print(serializer.data)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self,request):
+    def put(self, request):
+        pos_user = Pos_User.objects.get(user=request.user.id)
+        pos_user.latitude = request.data.get("latitude")
+        pos_user.longitude = request.data.get("longitude")
+
+        pos_user.save()
+        serializer = PositionSerializer(pos_user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class StationsView(APIView):
+    serializer_class = StationSerializer
+
+    def get(self, request):
+        station = Station.objects.all()
+        serializer = StationSerializer(station, many=True)
+
+        print(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
         if request.user.is_superuser:
-            serializer=StationSerializer(data=request.data)
+            serializer = StationSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data,status=status.HTTP_201_CREATED)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
             else:
-                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        return Response({"U are not Admin"},status=status.HTTP_400_BAD_REQUEST)
-    
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"U are not Admin"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class StationView(APIView):
-    serializer_class=StationSerializer
-    def get(self,request,pk):
-        station=get_object_or_404(Station,id=pk)
+    serializer_class = StationSerializer
+
+    def get(self, request, pk):
+        station = get_object_or_404(Station, id=pk)
 
         # station = Station.objects.get(id=pk)
-        serializer=StationSerializer(station)
-        data=serializer.data
-        data["stock"]=velos = len(station.velos.all())
-        
+        serializer = StationSerializer(station)
+        data = serializer.data
+        data["stock"] = velos = len(station.velos.all())
+
         print(serializer.data)
-        return Response(data,status=status.HTTP_200_OK)
-    def delete(self,request,pk):
+        return Response(data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
         if request.user.is_superuser:
-            station=get_object_or_404(Station,id=pk)
+            station = get_object_or_404(Station, id=pk)
             station.delete()
-            return Response("delete succefuly",status=status.HTTP_204_NO_CONTENT)
-        return Response({"U are not Admin"},status=status.HTTP_400_BAD_REQUEST)
-    def put(self,request,pk):
+            return Response("delete succefuly", status=status.HTTP_204_NO_CONTENT)
+        return Response({"U are not Admin"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
         if request.user.is_superuser:
-            station=get_object_or_404(Station,id=pk)
-            serializer=StationSerializer(station,data=request.data)
+            station = get_object_or_404(Station, id=pk)
+            serializer = StationSerializer(station, data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data,status=status.HTTP_200_OK)
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        return Response({"U are not Admin"},status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"U are not Admin"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(('GET',))
 def get_pos(request):
     if request.user.is_superuser:
         print(request.headers.get('Authorization'))
-        pos_users=Pos_User.objects.all()
-        serializer=PositionSerializer(pos_users,many=True)
+        pos_users = Pos_User.objects.all()
+        serializer = PositionSerializer(pos_users, many=True)
         return Response(serializer.data)
     return Response({"u are not admin"})
-
-
--- api/models.py
+++ api/models.py
@@ -1,35 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class User(AbstractUser):
-    sold=models.IntegerField(default=10)
+    matricule = models.CharField(max_length=12)
+    sold = models.IntegerField(default=10)
+
     def __str__(self):
         return self.username
 
+
 class Pos_User(models.Model):
-    user=models.OneToOneField(User,related_name="pos_of_user",on_delete=models.CASCADE)
-    latitude=models.FloatField(default=0)
-    longitude=models.FloatField(default=0)
+    user = models.OneToOneField(User, related_name="pos_of_user", on_delete=models.CASCADE)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
     def __str__(self):
-        return "position: "+ str(self.user)
+        return "position: " + str(self.user)
 
 
 class Station(models.Model):
-    name=models.CharField(max_length=50)
-    latitude=models.FloatField()
-    longitude=models.FloatField()
+    name = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
     def __str__(self):
         return self.name
+
 
 class Velo(models.Model):
     # lock=models.CharField(max_length=50)    
     station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='velos')
-    state=models.CharField(max_length=15)
-    latitude=models.FloatField()
-    longitude=models.FloatField()
+    state = models.CharField(max_length=15)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     # date_open=models.DateTimeField()
     # date_close=models.DateTimeField()
 
-    
